d365879dc4927022c00704cebb1d7feb6341a09a
f1i3g3 <dmitrvlkuznetsov@gmail.com>
2022-10-25 19:47:27 +0300

    Ocamlformat again
    Signed-off-by: f1i3g3 <dmitrvlkuznetsov@gmail.com>

--- CSharpAsync/demos/demoOnlyPrint.ml
+++ CSharpAsync/demos/demoOnlyPrint.ml
@@ -1,57 +1,44 @@
 open Csharp_lib.Interpreter_tests
 
-let s = "class Program \
-{
-    static int Primes(int a, int b) {
-        Console.WriteLine(\"Running primes on [a;b]=\"); \
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-        Console.WriteLine(\"->\");
-
-        int count = 0;
-        int isprime = 1;
-        int primes[42];
-
-        for (int i = a; i <= b; i++) {
-            isprime = 1;
-
-            for (int c = 2; c <= i / 2; c++) 
-               if (i % c == 0) {
-                   isprime = 0;
-               }
-            if (isprime == 1) {
-                    Console.WriteLine(i);
-                    count++;
-            }
-        }
-
-        Console.WriteLine(\"arr test\");
-
-        for (int i = 0; i < 10; i++) {
-            primes[i++] = count;
-        }
-
-        Console.WriteLine(primes[0]);
-        Console.WriteLine(primes[1]);
-    
-        return count;
-     }
-
-     static async void Go() {
-        int x = await(Primes(2, 10))  ;
-        Console.WriteLine(\"NPrimes=\"); 
-
-        Console.WriteLine(x); 
-    }
-
-
-    static void Main() \
-    { \
-        Console.WriteLine(\"main()\"); \
-        Go();
-       
-    } \
-}" (*Stdio.In_channel.input_all stdin*)
+let s =
+  "class Program {\n\
+  \    static int Primes(int a, int b) {\n\
+  \        Console.WriteLine(\"Running primes on [a;b]=\"); \
+   Console.WriteLine(a);\n\
+  \        Console.WriteLine(b);\n\
+  \        Console.WriteLine(\"->\");\n\n\
+  \        int count = 0;\n\
+  \        int isprime = 1;\n\
+  \        int primes[42];\n\n\
+  \        for (int i = a; i <= b; i++) {\n\
+  \            isprime = 1;\n\n\
+  \            for (int c = 2; c <= i / 2; c++) \n\
+  \               if (i % c == 0) {\n\
+  \                   isprime = 0;\n\
+  \               }\n\
+  \            if (isprime == 1) {\n\
+  \                    Console.WriteLine(i);\n\
+  \                    count++;\n\
+  \            }\n\
+  \        }\n\n\
+  \        Console.WriteLine(\"arr test\");\n\n\
+  \        for (int i = 0; i < 10; i++) {\n\
+  \            primes[i++] = count;\n\
+  \        }\n\n\
+  \        Console.WriteLine(primes[0]);\n\
+  \        Console.WriteLine(primes[1]);\n\
+  \    \n\
+  \        return count;\n\
+  \     }\n\n\
+  \     static async void Go() {\n\
+  \        int x = await(Primes(2, 10))  ;\n\
+  \        Console.WriteLine(\"NPrimes=\"); \n\n\
+  \        Console.WriteLine(x); \n\
+  \    }\n\n\n\
+  \    static void Main() { Console.WriteLine(\"main()\"); Go();\n\
+  \       \n\
+  \    } }"
+(*Stdio.In_channel.input_all stdin*)
 
 (*        int x = await (Test(1, 100) + 1 + await (await (2) + 3) + await (4 + 5));  *)
 let () = interpret s false
