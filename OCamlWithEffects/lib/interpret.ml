43963e2106fca2c3bb15bc5aa53f07cba7512e89
airh4ck <dudnikoff98@gmail.com>
2022-11-04 16:41:07 +0300

    Implement literal interpretation

--- OCamlWithEffects/lib/interpret.ml
+++ OCamlWithEffects/lib/interpret.ml
@@ -7,6 +7,8 @@ module type MONAD_FAIL = sig
   val ( let* ) : ('a, 'e) t -> ('a -> ('b, 'e) t) -> ('b, 'e) t
 end
 
+type error_message = string
+
 type environment = (id, value, id -> id -> bool) Base.Map.t
 
 and value =
@@ -18,3 +20,19 @@ and value =
   | VTuple of value list
   | VFun of expression * environment
   | VADT of data_constructor_name * value list
+
+module Interpret (M : MONAD_FAIL) : sig
+  val run : expression list -> ('a, error_message) M.t
+end = struct
+  let eval expression environment =
+    match expression with
+    | ELiteral literal ->
+      (match literal with
+       | LInt x -> VInt x
+       | LString x -> VString x
+       | LBool x -> VBool x
+       | LChar x -> VChar x)
+  ;;
+
+  let run _ = M.fail "Not implemented"
+end
