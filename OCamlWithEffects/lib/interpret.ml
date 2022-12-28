3b5fbd3dac7e3cade32ada00e0519b2778425ac4
Danila-Pechenev <danil-pechenev@mail.ru>
2022-12-28 15:30:40 +0300

    Fix comment
    Signed-off-by: Danila-Pechenev <danil-pechenev@mail.ru>

--- OCamlWithEffects/lib/interpret.ml
+++ OCamlWithEffects/lib/interpret.ml
@@ -15,7 +15,7 @@ type error =
   | Unreachable
       (** Unreachable code. If this error is thrown then something went seriously wrong *)
   | UnsupportedOperation (** Used unsupported operation *)
-  | Division_by_zero (** n / 0*)
+  | Division_by_zero (** n / 0 *)
   | NotAFunction (** Unreachable when type inference is used *)
   | TypeMismatch (** Unreachable when type inference is used *)
   | MisusedWildcard (** Wildcard is in the right-hand expression *)
@@ -108,11 +108,11 @@ end = struct
     match expression with
     | ELiteral literal ->
       (match literal with
-       | LInt x -> return @@ VInt x
-       | LString x -> return @@ VString x
-       | LBool x -> return @@ VBool x
-       | LChar x -> return @@ VChar x
-       | LUnit -> return VUnit)
+      | LInt x -> return @@ VInt x
+      | LString x -> return @@ VString x
+      | LBool x -> return @@ VBool x
+      | LChar x -> return @@ VChar x
+      | LUnit -> return VUnit)
     | EBinaryOperation (operation, left_operand, right_operand) ->
       let* left_operand = eval left_operand environment in
       let* right_operand = eval right_operand environment in
@@ -136,53 +136,51 @@ end = struct
         | _ -> fail Unreachable
       in
       (match operation, left_operand, right_operand with
-       (* Operations on ADT *)
-       | Eq, VADT (_, _), VADT (_, _) ->
-         vadt_predicate left_operand right_operand
-         >>= fun result -> return @@ VBool result
-       | NEq, VADT (x_name, x_data), VADT (y_name, y_data) ->
-         vadt_predicate (VADT (x_name, x_data)) (VADT (y_name, y_data))
-         >>= fun x -> return @@ VBool (not x)
-       | _, VADT (_, _), VADT (_, _) -> fail UnsupportedOperation
-       (* Arithmetic operations *)
-       | Add, VInt x, VInt y -> return @@ VInt (x + y)
-       | Sub, VInt x, VInt y -> return @@ VInt (x - y)
-       | Mul, VInt x, VInt y -> return @@ VInt (x * y)
-       | Div, VInt x, VInt y ->
-         if y = 0 then fail Division_by_zero else return @@ VInt (x / y)
-       | (Add | Sub | Mul | Div), _, _ -> fail TypeMismatch
-       (* And ( && ) *)
-       | AND, VBool x, VBool y -> return @@ VBool (x && y)
-       | OR, VBool x, VBool y -> return @@ VBool (x || y)
-       | (AND | OR), _, _ -> fail TypeMismatch
-       (* Equality *)
-       | op, arg1, arg2 ->
-         let comparison_operation : 'a. 'a -> 'a -> bool =
-           match op with
-           | Eq -> Base.Poly.( = )
-           | NEq -> Base.Poly.( <> )
-           | GT -> Base.Poly.( > )
-           | LT -> Base.Poly.( < )
-           | GTE -> Base.Poly.( >= )
-           | _ -> Base.Poly.( <= )
-         in
-         (match arg1, arg2 with
-          | VInt x, VInt y -> return @@ VBool (comparison_operation x y)
-          | VString x, VString y -> return @@ VBool (comparison_operation x y)
-          | VBool x, VBool y -> return @@ VBool (comparison_operation x y)
-          | VChar x, VChar y -> return @@ VBool (comparison_operation x y)
-          | VList x, VList y -> return @@ VBool (comparison_operation x y)
-          | _, _ -> fail UnsupportedOperation))
+      (* Operations on ADT *)
+      | Eq, VADT (_, _), VADT (_, _) ->
+        vadt_predicate left_operand right_operand >>= fun result -> return @@ VBool result
+      | NEq, VADT (x_name, x_data), VADT (y_name, y_data) ->
+        vadt_predicate (VADT (x_name, x_data)) (VADT (y_name, y_data))
+        >>= fun x -> return @@ VBool (not x)
+      | _, VADT (_, _), VADT (_, _) -> fail UnsupportedOperation
+      (* Arithmetic operations *)
+      | Add, VInt x, VInt y -> return @@ VInt (x + y)
+      | Sub, VInt x, VInt y -> return @@ VInt (x - y)
+      | Mul, VInt x, VInt y -> return @@ VInt (x * y)
+      | Div, VInt x, VInt y ->
+        if y = 0 then fail Division_by_zero else return @@ VInt (x / y)
+      | (Add | Sub | Mul | Div), _, _ -> fail TypeMismatch
+      (* And ( && ) *)
+      | AND, VBool x, VBool y -> return @@ VBool (x && y)
+      | OR, VBool x, VBool y -> return @@ VBool (x || y)
+      | (AND | OR), _, _ -> fail TypeMismatch
+      (* Equality *)
+      | op, arg1, arg2 ->
+        let comparison_operation : 'a. 'a -> 'a -> bool =
+          match op with
+          | Eq -> Base.Poly.( = )
+          | NEq -> Base.Poly.( <> )
+          | GT -> Base.Poly.( > )
+          | LT -> Base.Poly.( < )
+          | GTE -> Base.Poly.( >= )
+          | _ -> Base.Poly.( <= )
+        in
+        (match arg1, arg2 with
+        | VInt x, VInt y -> return @@ VBool (comparison_operation x y)
+        | VString x, VString y -> return @@ VBool (comparison_operation x y)
+        | VBool x, VBool y -> return @@ VBool (comparison_operation x y)
+        | VChar x, VChar y -> return @@ VBool (comparison_operation x y)
+        | VList x, VList y -> return @@ VBool (comparison_operation x y)
+        | _, _ -> fail UnsupportedOperation))
     | EIdentifier name ->
       if name = "_"
       then fail MisusedWildcard
       else
         let* v = find environment.ids name in
         (match v with
-         | VFun (id_list, function_body, environment, Recursive) ->
-           return
-           @@ VFun (id_list, function_body, update_ids environment name v, Recursive)
-         | _ -> return v)
+        | VFun (id_list, function_body, environment, Recursive) ->
+          return @@ VFun (id_list, function_body, update_ids environment name v, Recursive)
+        | _ -> return v)
     | EApplication (function_expr, argument_expr) ->
       let* eval_argument = eval argument_expr environment in
       let* eval_function = eval function_expr environment in
@@ -206,57 +204,57 @@ end = struct
       else return @@ VFun (id_list, function_body, environment, recursive)
     | EFun (arguments_list, function_body) ->
       (match arguments_list with
-       | [] -> eval function_body environment
-       | _ -> return @@ VFun (arguments_list, function_body, environment, NonRecursive))
+      | [] -> eval function_body environment
+      | _ -> return @@ VFun (arguments_list, function_body, environment, NonRecursive))
     | EDeclaration (_, arguments_list, function_body) ->
       (match arguments_list with
-       | [] -> eval function_body environment
-       | _ -> return @@ VFun (arguments_list, function_body, environment, NonRecursive))
+      | [] -> eval function_body environment
+      | _ -> return @@ VFun (arguments_list, function_body, environment, NonRecursive))
     | ERecursiveDeclaration (_, arguments_list, function_body) ->
       (match arguments_list with
-       | [] -> eval function_body environment
-       | _ -> return @@ VFun (arguments_list, function_body, environment, Recursive))
+      | [] -> eval function_body environment
+      | _ -> return @@ VFun (arguments_list, function_body, environment, Recursive))
     | EIf (condition, true_branch, false_branch) ->
       let* eval_conditional = eval condition environment in
       (match eval_conditional with
-       | VBool true -> eval true_branch environment
-       | VBool false -> eval false_branch environment
-       | _ -> fail TypeMismatch)
+      | VBool true -> eval true_branch environment
+      | VBool false -> eval false_branch environment
+      | _ -> fail TypeMismatch)
     | EUnaryOperation (operator, operand) ->
       let* operand = eval operand environment in
       (match operator, operand with
-       | Minus, VInt x -> return @@ VInt (-x)
-       | Not, VBool x -> return @@ VBool (not x)
-       | _ -> fail TypeMismatch)
+      | Minus, VInt x -> return @@ VInt (-x)
+      | Not, VBool x -> return @@ VBool (not x)
+      | _ -> fail TypeMismatch)
     | EList list ->
       (match list with
-       | [] -> return @@ VList []
-       | _ ->
-         let rec eval_list list =
-           match Base.List.hd_exn list, Base.List.tl_exn list with
-           | head, [] ->
-             let* head = eval head environment in
-             return @@ VList [ head ]
-           | head, tail ->
-             let* head = eval head environment in
-             let* tail = eval_list tail in
-             (match tail with
-              | VList tail -> return @@ VList (head :: tail)
-              | _ -> fail Unreachable)
-         in
-         eval_list list)
+      | [] -> return @@ VList []
+      | _ ->
+        let rec eval_list list =
+          match Base.List.hd_exn list, Base.List.tl_exn list with
+          | head, [] ->
+            let* head = eval head environment in
+            return @@ VList [ head ]
+          | head, tail ->
+            let* head = eval head environment in
+            let* tail = eval_list tail in
+            (match tail with
+            | VList tail -> return @@ VList (head :: tail)
+            | _ -> fail Unreachable)
+        in
+        eval_list list)
     | EConstructList (operand, list) ->
       let* operand = eval operand environment in
       let* list = eval list environment in
       (match operand, list with
-       | x, VList list -> return @@ VList (x :: list)
-       | _ -> fail TypeMismatch)
+      | x, VList list -> return @@ VList (x :: list)
+      | _ -> fail TypeMismatch)
     | EDataConstructor (constructor_name, content) ->
       (match content with
-       | Some data ->
-         let* data = eval data environment in
-         return @@ VADT (constructor_name, Some data)
-       | None -> return @@ VADT (constructor_name, None))
+      | Some data ->
+        let* data = eval data environment in
+        return @@ VADT (constructor_name, Some data)
+      | None -> return @@ VADT (constructor_name, None))
     | ETuple list ->
       let* list = foldr (fun x xs -> x :: xs) [] list in
       return @@ VTuple list
@@ -265,11 +263,11 @@ end = struct
         | h :: t ->
           let* result = eval h environment in
           (match h with
-           | EDeclaration (name, _, _) ->
-             eval_bindings (update_ids environment name result) t
-           | ERecursiveDeclaration (name, _, _) ->
-             eval_bindings (update_ids environment name result) t
-           | _ -> fail Unreachable)
+          | EDeclaration (name, _, _) ->
+            eval_bindings (update_ids environment name result) t
+          | ERecursiveDeclaration (name, _, _) ->
+            eval_bindings (update_ids environment name result) t
+          | _ -> fail Unreachable)
         | _ -> eval expression environment
       in
       eval_bindings environment bindings_list
@@ -278,24 +276,24 @@ end = struct
         Base.List.fold_right
           case_list
           ~f:(fun (case, action) environment ->
-            let* environment = environment in
+            let* environment in
             match case with
             | EEffectPattern effect ->
               (match effect with
-               | EEffectNoArg name ->
-                 let* _ = find_effect environment.effects name in
-                 return @@ update_effect_handlers environment name (EFun ([], action))
-               | EEffectArg (name, expression) ->
-                 let* _ = find_effect environment.effects name in
-                 return
-                 @@ update_effect_handlers
-                      environment
-                      name
-                      (EFun
-                         ( [ "eFFectValuE" ]
-                         , EMatchWith (EIdentifier "eFFectValuE", [ expression, action ])
-                         ))
-               | _ -> fail NotAnEffect)
+              | EEffectNoArg name ->
+                let* _ = find_effect environment.effects name in
+                return @@ update_effect_handlers environment name (EFun ([], action))
+              | EEffectArg (name, expression) ->
+                let* _ = find_effect environment.effects name in
+                return
+                @@ update_effect_handlers
+                     environment
+                     name
+                     (EFun
+                        ( [ "eFFectValuE" ]
+                        , EMatchWith (EIdentifier "eFFectValuE", [ expression, action ])
+                        ))
+              | _ -> fail NotAnEffect)
             | _ -> return environment)
           ~init:(return environment)
       in
@@ -337,19 +335,19 @@ end = struct
         | VTuple matched_tuple, ETuple tuple -> helper environment (matched_tuple, tuple)
         | VList matched_list, EConstructList (head, tail) ->
           (match matched_list with
-           | matched_head :: matched_tail ->
-             let result, environment, head_success =
-               compare_patterns
-                 matched_head
-                 head
-                 (EFun ([ "_" ], ELiteral LUnit))
-                 environment
-             in
-             let monadic_execution, new_environment, tail_success =
-               compare_patterns (VList matched_tail) tail action environment
-             in
-             result *> monadic_execution, new_environment, head_success && tail_success
-           | [] -> fail PatternMatchingFailed, environment, false)
+          | matched_head :: matched_tail ->
+            let result, environment, head_success =
+              compare_patterns
+                matched_head
+                head
+                (EFun ([ "_" ], ELiteral LUnit))
+                environment
+            in
+            let monadic_execution, new_environment, tail_success =
+              compare_patterns (VList matched_tail) tail action environment
+            in
+            result *> monadic_execution, new_environment, head_success && tail_success
+          | [] -> fail PatternMatchingFailed, environment, false)
         | VADT (matched_name, value), EDataConstructor (name, expression) ->
           if matched_name <> name
           then fail PatternMatchingFailed, environment, false
@@ -380,13 +378,13 @@ end = struct
     | EPerform expression ->
       let* eval_expression = eval expression environment in
       (match eval_expression with
-       | VEffectNoArg effect_name ->
-         let* handler = find environment.effect_handlers effect_name in
-         eval handler environment
-       | VEffectArg (effect_name, argument) ->
-         let* handler = find environment.effect_handlers effect_name in
-         eval (EApplication (handler, argument)) environment
-       | _ -> fail NotAnEffect)
+      | VEffectNoArg effect_name ->
+        let* handler = find environment.effect_handlers effect_name in
+        eval handler environment
+      | VEffectArg (effect_name, argument) ->
+        let* handler = find environment.effect_handlers effect_name in
+        eval (EApplication (handler, argument)) environment
+      | _ -> fail NotAnEffect)
     | EContinue expression -> eval expression environment
     | EEffectPattern expression ->
       let* eval_expression = eval expression environment in
@@ -400,11 +398,11 @@ end = struct
       | head :: tail ->
         let* result = eval head environment in
         (match head with
-         | EDeclaration (name, _, _) -> helper (update_ids environment name result) tail
-         | ERecursiveDeclaration (name, _, _) ->
-           helper (update_ids environment name result) tail
-         | EEffectDeclaration (name, _) -> helper (update_effects environment name) tail
-         | _ -> fail Unreachable)
+        | EDeclaration (name, _, _) -> helper (update_ids environment name result) tail
+        | ERecursiveDeclaration (name, _, _) ->
+          helper (update_ids environment name result) tail
+        | EEffectDeclaration (name, _) -> helper (update_effects environment name) tail
+        | _ -> fail Unreachable)
       | _ -> return VUnit
     in
     helper environment program
