1737ccf09a94c3ab03850a13ec3e3ae1f5f4de96
Startsev <tozarin@mail.ru>
2022-12-30 02:32:39 +0300

    Updates ast
    Signed-off-by: Startsev <tozarin@mail.ru>

--- asm/lib/Ast.ml
+++ asm/lib/Ast.ml
@@ -15,6 +15,64 @@ type _ reg =
   | Reg64 : string -> asmreg64 reg
   | Reg128 : string -> asmreg128 reg
 
+type dyn_reg = Dyn : 'a reg -> dyn_reg [@@unboxed]
+type const = ASMConst : string -> const
+type asmvar = ASMVar : string -> asmvar
+type label = ASMLabel : string -> label
+
+type expr =
+  | Add : expr * expr -> expr
+  | Sub : expr * expr -> expr
+  | Mul : expr * expr -> expr
+  | Div : expr * expr -> expr
+  | Const : const -> expr
+  | Var : asmvar -> expr
+
+type double_arg =
+  | RegToReg : 'a reg * 'a reg -> double_arg
+  | RegToExpr : _ reg * expr -> double_arg
+
+type single_arg = Reg : _ reg -> single_arg | Label : label -> single_arg
+
+type mnemonic =
+  | RET
+  | SYSCALL
+  | PUSH of single_arg
+  | POP of single_arg
+  | INC of single_arg
+  | DEC of single_arg
+  | NOT of single_arg
+  | NEG of single_arg
+  | JMP of single_arg
+  | JE of single_arg
+  | JNE of single_arg
+  | JZ of single_arg
+  | JG of single_arg
+  | JGE of single_arg
+  | JL of single_arg
+  | JLE of single_arg
+  | MOV of double_arg
+  | ADD of double_arg
+  | SUB of double_arg
+  | IMUL of double_arg
+  | AND of double_arg
+  | XOR of double_arg
+  | OR of double_arg
+  | SHL of double_arg
+  | SHR of double_arg
+  | CMP of double_arg
+
+type code_section = Command of mnemonic | Id of label
+
+type data_type = DB | DW | DD | DQ | DT
+[@@deriving show { with_path = false }]
+
+type var = Variable of string * data_type * string list
+[@@deriving show { with_path = false }]
+
+type dir = Code of code_section list | Data of var list
+type ast = Ast of dir list
+
 let show_reg : type a. a reg -> string = function
   | Reg8 x -> String.concat "" [ "(Reg8 \""; x; "\")" ]
   | Reg16 x -> String.concat "" [ "(Reg16 \""; x; "\")" ]
@@ -22,31 +80,18 @@ let show_reg : type a. a reg -> string = function
   | Reg64 x -> String.concat "" [ "(Reg64 \""; x; "\")" ]
   | Reg128 x -> String.concat "" [ "(Reg128 \""; x; "\")" ]
 
-type asmlabel
-type _ const = ASMConst : string -> _ const
-type _ asmvar = ASMVar : string -> _ asmvar
-type _ label = ASMLabel : string -> asmlabel label
+let show_dyn_reg (Dyn x) = String.concat "" [ "(Dyn "; show_reg x; ")" ]
 
-let show_label : type a. a label -> string = function
+let show_label : label -> string = function
   | ASMLabel x -> String.concat "" [ "(ASMLabel \""; x; "\")" ]
 
-let show_const : type a. a const -> string = function
+let show_const : const -> string = function
   | ASMConst x -> String.concat "" [ "(ASMConst \""; x; "\")" ]
 
-let show_asmvar : type a. a asmvar -> string = function
+let show_asmvar : asmvar -> string = function
   | ASMVar x -> String.concat "" [ "(ASMVar \""; x; "\")" ]
 
-type _ expr =
-  | Add : 'a expr * 'a expr -> 'a expr
-  | Sub : 'a expr * 'a expr -> 'a expr
-  | Mul : 'a expr * 'a expr -> 'a expr
-  | Div : 'a expr * 'a expr -> 'a expr
-  | Reg : 'a reg -> 'a expr
-  | Const : 'a const -> 'a expr
-  | Var : 'a asmvar -> 'a expr
-  | Label : 'a label -> 'a expr
-
-let rec show_expr : type a. a expr -> string = function
+let rec show_expr : expr -> string = function
   | Add (x, y) ->
       String.concat "" [ "(Add "; show_expr x; " "; show_expr y; ")" ]
   | Sub (x, y) ->
@@ -55,67 +100,79 @@ let rec show_expr : type a. a expr -> string = function
       String.concat "" [ "(Mul "; show_expr x; " "; show_expr y; ")" ]
   | Div (x, y) ->
       String.concat "" [ "(Div "; show_expr x; " "; show_expr y; ")" ]
-  | Reg x -> String.concat "" [ "(Reg "; show_reg x; ")" ]
   | Const x -> String.concat "" [ "(Const "; show_const x; ")" ]
   | Var x -> String.concat "" [ "(Var "; show_asmvar x; ")" ]
+
+let show_double_arg = function
+  | RegToReg (x, y) ->
+      String.concat "" [ "(RegToReg "; show_reg x; " "; show_reg y; ")" ]
+  | RegToExpr (x, y) ->
+      String.concat "" [ "(RegToExpr "; show_reg x; " "; show_expr y; ")" ]
+
+let show_single_arg = function
+  | Reg x -> String.concat "" [ "(Reg "; show_reg x; ")" ]
   | Label x -> String.concat "" [ "(Label "; show_label x; ")" ]
 
-type mnemonic = Mnemonic of string [@@deriving show { with_path = false }]
-
-type command =
-  | Args0 : mnemonic -> command
-  | Args1 : mnemonic * 'a expr -> command
-  | Args2 : mnemonic * 'a expr * 'a expr -> command
-
-let show_command : command -> string = function
-  | Args0 x -> String.concat "" [ "(Args0 "; show_mnemonic x; ")" ]
-  | Args1 (x, y) ->
-      String.concat "" [ "(Args1 "; show_mnemonic x; " "; show_expr y; ")" ]
-  | Args2 (x, y, z) ->
-      String.concat ""
-        [ "(Args2 "; show_mnemonic x; " "; show_expr y; " "; show_expr z; ")" ]
-
-type code_section = Command of command | Id of asmlabel label
+let show_mnemonic : mnemonic -> string = function
+  | RET -> "(RET)"
+  | SYSCALL -> "(SYSCALL)"
+  | PUSH x -> String.concat "" [ "(PUSH "; show_single_arg x; ")" ]
+  | POP x -> String.concat "" [ "(POP "; show_single_arg x; ")" ]
+  | INC x -> String.concat "" [ "(INC "; show_single_arg x; ")" ]
+  | DEC x -> String.concat "" [ "(DEC "; show_single_arg x; ")" ]
+  | NOT x -> String.concat "" [ "(NOT "; show_single_arg x; ")" ]
+  | NEG x -> String.concat "" [ "(NEG "; show_single_arg x; ")" ]
+  | JMP x -> String.concat "" [ "(JMP "; show_single_arg x; ")" ]
+  | JE x -> String.concat "" [ "(JE "; show_single_arg x; ")" ]
+  | JNE x -> String.concat "" [ "(JNE "; show_single_arg x; ")" ]
+  | JZ x -> String.concat "" [ "(JZ "; show_single_arg x; ")" ]
+  | JG x -> String.concat "" [ "(JG "; show_single_arg x; ")" ]
+  | JGE x -> String.concat "" [ "(JGE "; show_single_arg x; ")" ]
+  | JL x -> String.concat "" [ "(JL "; show_single_arg x; ")" ]
+  | JLE x -> String.concat "" [ "(JLE "; show_single_arg x; ")" ]
+  | MOV x -> String.concat "" [ "(MOV "; show_double_arg x; ")" ]
+  | ADD x -> String.concat "" [ "(ADD "; show_double_arg x; ")" ]
+  | SUB x -> String.concat "" [ "(SUB "; show_double_arg x; ")" ]
+  | IMUL x -> String.concat "" [ "(IMUL "; show_double_arg x; ")" ]
+  | AND x -> String.concat "" [ "(AND "; show_double_arg x; ")" ]
+  | XOR x -> String.concat "" [ "(XOR "; show_double_arg x; ")" ]
+  | OR x -> String.concat "" [ "(OR "; show_double_arg x; ")" ]
+  | SHL x -> String.concat "" [ "(SHL "; show_double_arg x; ")" ]
+  | SHR x -> String.concat "" [ "(SHR "; show_double_arg x; ")" ]
+  | CMP x -> String.concat "" [ "(CMP "; show_double_arg x; ")" ]
 
 let show_code_section : code_section -> string = function
-  | Command x -> String.concat "" [ "(Command "; show_command x; ")" ]
+  | Command x -> String.concat "" [ "(Command "; show_mnemonic x; ")" ]
   | Id x -> String.concat "" [ "(Id "; show_label x; ")" ]
-
-type data_type = DB | DW | DD | DQ | DT
-[@@deriving show { with_path = false }]
-
-type var = string * data_type * string list
-[@@deriving show { with_path = false }]
-
-type dir = Code of code_section list | Data of var list
 
 let show_dir : dir -> string = function
   | Code x ->
       String.concat ""
         [
+          "Code ";
           "[\n";
           List.fold_left
             (fun x y ->
-              String.concat "" [ x; "\t"; show_code_section y; ";\n" ])
+              String.concat "" [ x; "\t\t"; show_code_section y; ";\n" ])
             "" x;
           "]";
         ]
   | Data x ->
       String.concat ""
         [
+          "Data ";
           "[\n";
           List.fold_left
-            (fun x y -> String.concat "" [ x; "\t"; show_var y; "\n" ])
+            (fun x y -> String.concat "" [ x; "\t\t"; show_var y; "\n" ])
             "" x;
           "]";
         ]
 
-type ast = dir list
-
 let show_ast : ast -> string =
- fun x ->
+ fun (Ast x) ->
   String.concat ""
     [
+      "(Ast ";
       "[\n";
       List.fold_left
         (fun x y -> String.concat "" [ x; "\t"; show_dir y; ";\n" ])
