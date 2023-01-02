0207b649d002fdbab93b3970d8298c50e1504622
airh4ck <dudnikoff98@gmail.com>
2023-01-02 14:25:26 +0300

    Add tests for AST pretty-printer
    Signed-off-by: airh4ck <dudnikoff98@gmail.com>

--- OCamlWithEffects/lib/Pprintast.ml
+++ OCamlWithEffects/lib/Pprintast.ml
@@ -40,15 +40,18 @@ let rec pp_expression fmt =
     | ELiteral _ | EIdentifier _ -> format_of_string "%a"
     | _ -> format_of_string "(%a)"
   in
-  let rec pp_list fmt delimiter = function
-    | [ head ] -> pp_expression fmt head
-    | head :: tail ->
-      pp_expression fmt head;
-      fprintf fmt "%c " delimiter;
-      pp_list fmt delimiter tail
-    | [] -> ()
+  let pp_list fmt delimiter =
+    pp_print_list
+      ~pp_sep:(fun fmt _ -> fprintf fmt delimiter)
+      (fun fmt value -> pp_expression fmt value)
+      fmt
   in
-  let pp_id_list fmt list = List.iter list ~f:(fun x -> fprintf fmt "%s;" x) in
+  let pp_id_list fmt =
+    pp_print_list
+      ~pp_sep:(fun fmt _ -> fprintf fmt " ")
+      (fun fmt value -> fprintf fmt "%s" value)
+      fmt
+  in
   function
   | ELiteral literal -> pp_literal fmt literal
   | EIdentifier id -> fprintf fmt "%s" id
@@ -89,16 +92,16 @@ let rec pp_expression fmt =
       right_operand
   | EFun (id_list, expression) ->
     fprintf fmt "fun %a -> %a" pp_id_list id_list pp_expression expression
-  | EList list -> pp_list fmt ';' list
+  | EList list -> fprintf fmt "[%a]" (fun fmt -> pp_list fmt "; ") list
   | EConstructList (operand, list) ->
     fprintf
       fmt
-      (get_format operand ^^ get_format list)
+      (get_format operand ^^ " :: " ^^ get_format list)
       pp_expression
       operand
       pp_expression
       list
-  | ETuple list -> pp_list fmt ',' list
+  | ETuple list -> pp_list fmt ", " list
   | EDeclaration (name, id_list, expression) ->
     fprintf fmt "let %s %a = %a" name pp_id_list id_list pp_expression expression
   | ERecursiveDeclaration (name, id_list, expression) ->
@@ -120,9 +123,12 @@ let rec pp_expression fmt =
   | EEffectDeclaration (name, typ) -> fprintf fmt "effect %s : %a" name pp_type typ
   | EEffectNoArg name -> fprintf fmt "%s" name
   | EEffectArg (name, expression) -> fprintf fmt "%s %a" name pp_expression expression
-  | EPerform expression -> fprintf fmt "perform %a" pp_expression expression
-  | EContinue expression -> fprintf fmt "continue %a" pp_expression expression
-  | EEffectPattern expression -> fprintf fmt "effect %a" pp_expression expression
+  | EPerform expression ->
+    fprintf fmt ("perform " ^^ get_format expression) pp_expression expression
+  | EContinue expression ->
+    fprintf fmt ("continue " ^^ get_format expression) pp_expression expression
+  | EEffectPattern expression ->
+    fprintf fmt ("effect " ^^ get_format expression) pp_expression expression
   | ELetIn (expression_list, expression) ->
     (match expression_list with
      | [] -> fprintf fmt "Parsing error."
@@ -138,4 +144,71 @@ let rec pp_expression fmt =
     fprintf fmt "match %a with" pp_expression matched_expression;
     List.iter case_list ~f:(fun (case, action) ->
       fprintf fmt " | %a -> %a" pp_expression case pp_expression action)
+;;
+
+let%expect_test _ =
+  printf "%a" pp_expression
+  @@ EFun
+       ( [ "x"; "y"; "z" ]
+       , EMatchWith
+           ( ETuple [ EIdentifier "x"; EIdentifier "y"; EIdentifier "z" ]
+           , [ ( ETuple
+                   [ ELiteral (LBool true)
+                   ; ELiteral (LBool true)
+                   ; ELiteral (LBool false)
+                   ]
+               , ELiteral (LBool true) )
+             ; ( ETuple
+                   [ ELiteral (LBool true)
+                   ; ELiteral (LBool false)
+                   ; ELiteral (LBool true)
+                   ]
+               , ELiteral (LBool true) )
+             ; ( ETuple
+                   [ ELiteral (LBool false)
+                   ; ELiteral (LBool true)
+                   ; ELiteral (LBool true)
+                   ]
+               , ELiteral (LBool true) )
+             ; EIdentifier "_", ELiteral (LBool false)
+             ] ) );
+  [%expect
+    {|
+  fun x y z -> match x, y, z with | true, true, false -> true | true, false, true -> true | false, true, true -> true | _ -> false
+  |}]
+;;
+
+let%expect_test _ =
+  printf "%a" pp_expression
+  @@ EFun
+       ( [ "line"; "number"; "line_mult_number" ]
+       , EMatchWith
+           ( EIdentifier "line"
+           , [ ( EConstructList (EIdentifier "head", EIdentifier "tail")
+               , EConstructList
+                   ( EBinaryOperation (Mul, EIdentifier "head", EIdentifier "number")
+                   , EApplication (EIdentifier "line_mult_number", EIdentifier "tail") ) )
+             ; EIdentifier "_", EList []
+             ] ) );
+  [%expect
+    {|
+  fun line number line_mult_number -> match line with | head :: tail -> (head * number) :: (line_mult_number tail) | _ -> []
+  |}]
+;;
+
+let%expect_test _ =
+  printf "%a" pp_expression
+  @@ EDeclaration
+       ( "helper"
+       , [ "x" ]
+       , EMatchWith
+           ( EPerform (EEffectArg ("E", EIdentifier "x"))
+           , [ ( EEffectPattern (EEffectArg ("E", EIdentifier "s"))
+               , EContinue (EBinaryOperation (Mul, EIdentifier "s", EIdentifier "s")) )
+             ; EIdentifier "l", EIdentifier "l"
+             ] ) );
+  [%expect
+    {|
+    let helper x = match perform (E x) with | effect (E s) -> continue (s * s) | l -> l
+  |}]
 ;;
