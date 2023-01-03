058c32d876524e371bf225378c7fac37e62d6cce
Startsev <tozarin@mail.ru>
2023-01-03 20:40:26 +0300

    Fixes interpret
    Signed-off-by: Startsev <tozarin@mail.ru>

--- asm/lib/interpreter.ml
+++ asm/lib/interpreter.ml
@@ -82,7 +82,7 @@ module Interpret (M : MONADERROR) = struct
   let prep = function [] -> MapVar.empty | l -> MapVar.of_seq (List.to_seq l)
 
   let full_name : type a. a reg -> string t = function
-    | Reg8 x -> return (Printf.sprintf "R%cX" x.[1])
+    | Reg8 x -> return (Printf.sprintf "R%cX" x.[0])
     | Reg16 x -> return (Printf.sprintf "R%s" x)
     | Reg32 x -> return (Printf.sprintf "R%c%c" x.[1] x.[2])
     | Reg64 x -> return x
@@ -99,10 +99,9 @@ module Interpret (M : MONADERROR) = struct
         | Reg8 name ->
             return
             @@
-            if rreg name then logand x 0xFFL
-            else shift_left (logand x 0xFFFFL) 8
-        | Reg16 _ -> return @@ logand x 0xFFFFL
-        | Reg32 _ -> return @@ logand x 0xFFFFFFFFL
+            if rreg name then rem x 0x1FFL else shift_right (rem x 0x1FFFFL) 8
+        | Reg16 _ -> return @@ rem x 0x1FFFFL
+        | Reg32 _ -> return @@ rem x 0x1FFFFFFFFL
         | Reg64 _ -> return x
         | _ -> error "Isnt reg64 or less")
     | _ -> error "Isnt reg64 or less"
@@ -126,17 +125,21 @@ module Interpret (M : MONADERROR) = struct
   let change_reg64 :
       type a. var MapVar.t -> Int64.t -> a reg -> var MapVar.t M.t =
    fun env v ->
-    let mask v l r =
-      add (logand v (sub (shift_left 1L l) 1L)) (div v (shift_left 1L r))
-    in
     let insert ov v l r =
-      add
-        (sub ov (mask ov l r))
-        (logand (shift_left v r) (sub (shift_left v l) 1L))
+      let ones l r =
+        of_string
+        @@ Printf.sprintf "0b0%s%s"
+             (String.make (l - r) '1')
+             (String.make r '0')
+      in
+      let mask v l r = logand v (ones l r) in
+      let amask v l r = lognot (mask v l r) in
+      let inc = add 1L in
+      inc @@ add (amask ov l r) (shift_left (mask v (l - r) 0) r)
     in
     let f env reg v l r =
       full_name reg >>= fun name ->
-      find_r env (Reg64 name) >>= fun ov ->
+      find_r env reg >>= fun ov ->
       return @@ insert ov v l r >>= fun v ->
       return @@ MapVar.add name (Reg64 v) env
     in
@@ -165,22 +168,22 @@ module Interpret (M : MONADERROR) = struct
     | Const (ASMConst x) -> return @@ of_string x
     | Var (ASMVar x) -> find_v env x
 
-  let inter env code (h :: stl) (cmd :: tl) =
-    let s = h :: stl in
-    let f x af =
-      find_r env x >>= fun ov ->
-      return (af ov) >>= fun nv ->
-      change_reg64 env nv x >>= fun env ->
-      find_r env x >>= fun nnv ->
-      change_eflag env (nv = 0L) (nv < 0L) (nv != nnv) >>= fun env ->
-      return (env, s, tl)
-    in
+  let inter env code s cmds =
+    let tl = List.tl cmds in
     let rec assoc l = function
-      | [] -> error ""
+      | [] -> error "No such label"
       | Id label :: tl when label = l -> return tl
       | _ :: tl -> assoc l tl
     in
-    let rec c_flags env =
+    let f x af =
+      find_r env x >>= fun v ->
+      let nv = af v in
+      change_reg64 env nv x >>= fun env ->
+      find_r env x >>= fun nnv ->
+      change_eflag env (nv = 0L) (nv < 0L) (nv <> nnv) >>= fun env ->
+      return (env, s, tl)
+    in
+    let c_flags env =
       find_f env "ZF" >>= fun z ->
       find_f env "SF" >>= fun s ->
       find_f env "OF" >>= fun f -> return (z, s = f)
@@ -191,24 +194,24 @@ module Interpret (M : MONADERROR) = struct
       else return (env, s, tl)
     in
     let u_da = function
-      | RegToReg (x, y) -> find_r env y >>= fun y -> return (Dyn x, y)
       | RegToExpr (x, y) -> ev env y >>= fun y -> return (Dyn x, y)
+      | RegToReg (x, y) -> find_r env y >>= fun y -> return (Dyn x, y)
     in
     let ff x af = u_da x >>= fun (Dyn x, y) -> f x (af y) in
-    match cmd with
-    | Id _ -> return (env, s, tl)
-    | Command x -> (
+    match cmds with
+    | Id _ :: _ -> return (env, s, tl)
+    | Command x :: _ -> (
         match x with
         | RET -> return (env, s, [])
-        | SYSCALL -> return (env, s, tl) (*TODO*)
-        | PUSH x -> return (env, Dyn x :: s, tl)
+        | SYSCALL -> return (env, s, tl)
+        | PUSH x -> find_r env x >>= fun v -> return (env, v :: s, tl)
         | POP x ->
-            find_r env x >>= fun v ->
-            change_reg64 env v x >>= fun env -> return (env, stl, tl)
-        | INC x -> f x (add 1L)
-        | DEC x -> f x (sub 1L)
-        | NOT x -> f x lognot
-        | NEG x -> f x neg
+            change_reg64 env (List.hd s) x >>= fun env ->
+            return (env, List.tl s, tl)
+        | INC x -> f x (fun x -> add (mul x 2L) 1L)
+        | DEC x -> f x (fun x -> sub (mul x 2L) 1L)
+        | NOT x -> f x (fun x -> lognot (mul x 2L))
+        | NEG x -> f x (fun x -> neg (mul x 2L))
         | JMP x -> jmp x (fun _ -> true)
         | JE x | JZ x -> jmp x (fun (x, _) -> x)
         | JNE x -> jmp x (fun (x, _) -> not x)
@@ -216,21 +219,23 @@ module Interpret (M : MONADERROR) = struct
         | JGE x -> jmp x (fun (_, y) -> y)
         | JL x -> jmp x (fun (_, y) -> not y)
         | JLE x -> jmp x (fun (x, y) -> x & not y)
-        | MOV x -> ff x (fun x _ -> x)
-        | ADD x -> ff x add
-        | SUB x -> ff x sub
-        | IMUL x -> ff x mul
-        | AND x -> ff x logand
-        | XOR x -> ff x logxor
-        | OR x -> ff x logor
+        | MOV x -> ff x (fun x y -> add x y)
+        | ADD x -> ff x (fun x y -> add x (mul y 2L))
+        | SUB x -> ff x (fun x y -> mul (sub x (mul y 2L)) minus_one)
+        | IMUL x -> ff x (fun x y -> mul (add x 1L) y)
+        | AND x -> ff x (fun x y -> add (logand x y) y)
+        | OR x -> ff x (fun x y -> add (logor x y) y)
+        | XOR x -> ff x (fun x y -> add (logxor x y) y)
         | SHL (x, y) ->
-            ev env y >>= fun y -> f x (fun x -> shift_left x (to_int y))
+            ev env y >>= fun y -> f x (fun x -> add x (shift_left x (to_int y)))
         | SHR (x, y) ->
-            ev env y >>= fun y -> f x (fun x -> shift_right x (to_int y))
+            ev env y >>= fun y ->
+            f x (fun x -> add x (shift_right x (to_int y)))
         | CMP x ->
             u_da x >>= fun (Dyn x, y) ->
             find_r env x >>= fun x ->
             change_flag env "ZF" (x = y) >>= fun env -> return (env, s, tl))
+    | _ -> return (env, s, [])
 
   let rec code_sec_inter env s code = function
     | cmd :: tl ->
@@ -238,49 +243,15 @@ module Interpret (M : MONADERROR) = struct
         code_sec_inter env s code c
     | [] -> return (env, s, [])
 
-  (*
-     (** not implemeted data secction interpreter *)
-     let data_sec_inter env st = function _ -> return (env, st)
+  let data_sec_inter env s = function _ -> return (env, s, []) (*TODO*)
 
-     (** code section interpreter that return map, ast - general code that shouldn't change *)
-     let rec code_sec_inter env st ast =
-       let jump env ast tl = function
-         | Args1 (Mnemonic cmd, Lab label) ->
-             assoc label ast >>= fun code ->
-             (match cmd with
-             | "JMP" -> return true
-             | "JE" | "JZ" -> find_flag_cont env "ZF"
-             | "JNE" -> find_flag_cont env "ZF" >>= fun z -> return @@ not z
-             | "JG" -> find_eflags env >>= fun (z, s, o) -> return (o = s && not z)
-             | "JGE" -> find_eflags env >>= fun (_, s, o) -> return (o = s)
-             | "JL" -> find_eflags env >>= fun (_, s, o) -> return (o != s)
-             | "JLE" -> find_eflags env >>= fun (z, s, o) -> return (z && o != s)
-             | x -> error @@ x ^ " is not a jmp")
-             >>= fun cond ->
-             if cond then code_sec_inter env st ast code
-             else code_sec_inter env st ast tl
-         | _ -> error "Isnt jmp"
-       in
-       function
-       | Command cmd :: tl -> (
-           match cmd with
-           | Args1 (Mnemonic c, _) when is_jmp c -> jump env ast tl cmd
-           | _ ->
-               inter_cmd env st cmd >>= fun (env, st) ->
-               code_sec_inter env st ast tl)
-       | Id _ :: tl -> code_sec_inter env st ast tl
-       | [] -> return (env, st)
-
-     (** general general interpreter *)
-     let rec interpret env st = function
-       | [] -> return (env, st)
-       | h :: tl -> (
-           match h with
-           | Code code ->
-               t_ch code >>= fun _ ->
-               code_sec_inter env st code code >>= fun (env, st) ->
-               interpret env st tl
-           | _ -> error "Not implemented yet")
-     (* data_sec_inter st env data >>= fun (env, st) -> interpret env st tl) *)
-  *)
+  let rec interpret env s = function
+    | h :: tl -> (
+        match h with
+        | Code code ->
+            code_sec_inter env s code code >>= fun (env, s, _) ->
+            interpret env s tl
+        | Data data ->
+            data_sec_inter env s data >>= fun (env, s, _) -> interpret env s tl)
+    | [] -> return (env, s, [])
 end
