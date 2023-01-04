d1a3016c5572f9778bcc99d0c1d8d7d46f0e116b
Startsev <tozarin@mail.ru>
2023-01-04 18:32:02 +0300

    Updates REPL
    Signed-off-by: Startsev <tozarin@mail.ru>

--- asm/REPL.ml
+++ asm/REPL.ml
@@ -6,19 +6,20 @@ open Asm.Interpreter
 open Asm.Parser
 open Asm.Ast
 open Interpret (Result)
+open Int64
 
 let run_str env st code =
   match eval code with
-  | Parsed ast -> (
+  | Parsed (Ast ast) -> (
       match interpret env st ast with
-      | Ok (env, st) ->
-          print_endline @@ show_ast ast;
-          List.iter print_endline (List.map (fun (_, v) -> show_var v) st);
+      | Ok (env, st, _) ->
+          print_endline @@ show_ast (Ast ast);
+          List.iter print_endline (List.map to_string st);
           print_endline @@ show_envr env;
           (env, st)
       | Error msg ->
-          print_endline @@ show_ast ast;
-          List.iter print_endline (List.map (fun (_, v) -> show_var v) st);
+          print_endline @@ show_ast (Ast ast);
+          List.iter print_endline (List.map to_string st);
           print_endline msg;
           (env, st))
   | Failed msg ->
